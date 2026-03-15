6c4d688bc2af76e7bd4b8fe9e002002bbe9cb068
subhamrexpaul <subhampaulzz00@gmail.com>
2026-03-16 05:00:32 +0530

    test: add test_delete_without_token_returns_403

---
+++ tests/test_negative.py
@@ -0,0 +1,11 @@
+import pytest
+from src.utils.assertions import assert_status_code
+from src.utils.data_factory import generate_booking
+
+@pytest.mark.negative
+def test_delete_without_token_returns_403(booking_api, sample_booking):
+    """
+    Test that deleting a booking without a token returns a 403 Forbidden.
+    """
+    response = booking_api.delete_booking(sample_booking, token="")
+    assert_status_code(response, 403)
