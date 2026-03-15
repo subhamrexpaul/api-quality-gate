022aab0bff60de1623e2d3081fd1dee9cf94be1e
subhamrexpaul <subhampaulzz00@gmail.com>
2026-03-16 05:00:56 +0530

    test: add test_update_without_token_returns_403

--- tests/test_negative.py
+++ tests/test_negative.py
@@ -9,3 +9,14 @@ def test_delete_without_token_returns_403(booking_api, sample_booking):
     """
     response = booking_api.delete_booking(sample_booking, token="")
     assert_status_code(response, 403)
+
+
+@pytest.mark.negative
+def test_update_without_token_returns_403(booking_api, sample_booking):
+    """
+    Test that updating a booking without a token returns a 403 Forbidden.
+    """
+    updated_data = generate_booking()
+    response = booking_api.update_booking(sample_booking, updated_data, token="")
+    assert_status_code(response, 403)
+
